b9691f49a4f8ea9d38290e325f5980efdf814c9c
Anisur Rahman <anisoncloud@gmail.com>
2026-04-06 01:16:49 +0600

    Initial

---
+++ POS.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POS.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreaedAt { get; set; }= DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public string? CreatedBy {  get; set; }
+        public bool IsDeleted { get; set; }=false;
+    }
+}
