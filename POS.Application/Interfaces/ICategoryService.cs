129a51e971c2d82c6ccb8900018433a5d023609b
Anisur Rahman <anisoncloud@gmail.com>
2026-05-06 18:30:56 +0600

    Mapper

--- POS.Application/Interfaces/ICategoryService.cs
+++ POS.Application/Interfaces/ICategoryService.cs
@@ -10,13 +10,13 @@ namespace POS.Application.Interfaces
         Task<IEnumerable<CategoryDto>> GetAllAsync();
         Task<IEnumerable<CategoryDto>> GetAllWithParentAsync();
         Task<IEnumerable<CategoryDto>> GetTopLevelAsync();
-        Task<IEnumerable<CategoryDto>> GetSubCategoriesAsunc(int parentId);
+        Task<IEnumerable<CategoryDto>> GetSubCategoriesAsync(int parentId);
         Task<CategoryDto> GetByIdAsync(int id);
-        Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync();
+        /*Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync();
         Task<IEnumerable<CategoryDto>> GetAllSubCategoriesByIdAsync(int categoryId);
         Task<CategoryDto> GetCategoryByIdAsync(int categoryId);
-        Task<CategoryDto> GetCategoryByNameAsync(string name);
+        Task<CategoryDto> GetCategoryByNameAsync(string name);*/
         Task<CategoryDto> CreateCategoryAsync(CategoryCreateDto dto);
-        Task<CategoryDto> UpdateCategoryAsync(CategoryUpdateDto dto);
+        Task<CategoryDto> UpdateCategoryAsync(int id, CategoryUpdateDto dto);
     }
 }
--- POS.Application/Services/CategoryService.cs
+++ POS.Application/Services/CategoryService.cs
@@ -31,19 +31,57 @@ namespace POS.Application.Services
             return MapWithParentName(categories);
         }
 
+        //---Get All Categories with who have the parent category--------
         public async Task<IEnumerable<CategoryDto>> GetAllWithParentAsync()
         {
+            var categories = await _uow.Categories.GetAllWithParentAsync();
+                return MapWithParentName(categories);
+        }
 
+        //--- Get All Only Category who dont have any parent category id------
+        public async Task<IEnumerable<CategoryDto>> GetTopLevelAsync()
+        {
+            var categories = await _uow.Categories.GetTopLevelAsync();
+            return _mapper.Map<IEnumerable<CategoryDto>>(categories);
+        }
+        //------ Get All SubCategories with same parent id-------------
+        public async Task<IEnumerable<CategoryDto>> GetSubCategoriesAsync(int parentId)
+        {
+            var subCategories = await _uow.Categories.GetSubCategoriesAsync(parentId);
+            return _mapper.Map<IEnumerable<CategoryDto>>(subCategories);
+        }
+
+
+        //-----Get Only SubCategories of A Category------------------
+        public async Task<IEnumerable<CategoryDto>> GetCategoriesAsync(int parentId)
+        {
+            var subCategories = await _uow.Categories.GetSubCategoriesAsync(parentId);
+            return _mapper.Map<IEnumerable<CategoryDto>>(subCategories); 
+        }
+
+        //---Get A Category or SubCategory with parent by his Id-----
+        public async Task<CategoryDto> GetByIdAsync(int id)
+        {
+            var category = await _uow.Categories.GetByIdAsync(id);
+            if (category==null)
+            {
+                throw new KeyNotFoundException($"Category with Id {id} not found");
+            }
+            var dto = _mapper.Map<CategoryDto>(category);
+            if (category.ParentCategoryId.HasValue)
+            {
+                var parent = await _uow.Categories
+                    .GetByIdAsync(category.ParentCategoryId.Value);
+                dto.ParentCategoryName = parent?.Name;
+            }
+            return dto;
         }
 
         public async Task<CategoryDto> CreateCategoryAsync(CategoryCreateDto dto)
         {
             var existing = await _uow.Categories.GetByNameAsync(dto.Name);
             if (existing != null)
-            {
-                //var categories = await _uow.Categories.GetAllAsync();
-                //var dtos = _mapper.Map<IEnumerable<CategoryDto>>(categories).ToList();
-                //return dots;
+            {                
                 throw new InvalidOperationException(
                     $"A category with the name '{dto.Name}' is already exists"
                     );
@@ -53,30 +91,9 @@ namespace POS.Application.Services
             await _uow.Categories.AddAsync(category);
             await _uow.CommitAsync();
             return _mapper.Map<CategoryDto>(category);
-        }
-
-        /*public async Task<ServiceResult<CategoryDto>> CreateCategoryAsync(CategoryCreateDto dto)
-        {
-            var existing = _uow.Categories.GetByNameAsync(dto.Name);
-            if (existing !=null)
-            {
-                return new ServiceResult<CategoryDto>
-                {
-                    Success = false,
-                    Message = "Category already exists"
-                };
-            }
-            var category = _mapper.Map<Category>(dto);
-            await _uow.Categories.AddAsync(category);
-            await _uow.CommitAsync();
-            return new ServiceResult<CategoryDto>
-            {
-                Success = true,
-                Message = "Category Created Successfully"
-            };
-        }*/
-
-        public async Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync()
+        }        
+        //--- Get All Categories ---------------
+        /*public async Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync()
         {
             var categories = await _uow.Categories.GetAllAsync();
             var dots = _mapper.Map<IEnumerable<CategoryDto>>(categories).ToList();
@@ -98,9 +115,9 @@ namespace POS.Application.Services
         public Task<CategoryDto> GetCategoryByNameAsync(string name)
         {
             throw new NotImplementedException();
-        }
+        }*/
 
-        public Task<CategoryDto> UpdateCategoryAsync(CategoryUpdateDto dto)
+        public Task<CategoryDto> UpdateCategoryAsync(int id, CategoryUpdateDto dto)
         {
             throw new NotImplementedException();
         }
